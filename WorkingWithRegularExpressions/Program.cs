89648d211ae0044bb12f9e9d3a65c2ac29e5ef81
Jerome Jacob <Jerome56jj@hotmail.com>
2022-03-22 22:02:52 +0100

    RegEx

---
+++ WorkingWithRegularExpressions/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+using static System.Console;
+
+Write("Enter your age: ");
+string? input = ReadLine();
+
+Regex ageChecker = new(@"^\d+$");
+
+if (ageChecker.IsMatch(input))
+{
+    WriteLine("Thank you!");
+}
+else
+{
+    WriteLine($"This is not a valid age: {input}");
+}
--- WorkingWithTime/Program.cs
+++ WorkingWithTime/Program.cs
@@ -1,4 +1,5 @@
 ﻿using static System.Console;
+using System.Globalization;
 
 WriteLine("Earliest date/time value is: {0}",
     arg0: DateTime.MinValue);
@@ -28,3 +29,15 @@ WriteLine("Christmas is day {0} of the year.",
 WriteLine("Christmas {0} is on a {1}.",
     arg0: christmas.Year,
     arg1: christmas.DayOfWeek);
+
+WriteLine();
+
+WriteLine("Current culture is: {0}",
+    arg0: CultureInfo.CurrentCulture.Name);
+
+string textDate = "4 July 2021";
+DateTime independeceDay = DateTime.Parse(textDate);
+
+WriteLine("Text: {0}, DateTime: {1:d MMMM}",
+    arg0: textDate,
+    arg1: independeceDay);
