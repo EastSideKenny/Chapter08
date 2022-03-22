2ff14f263b0a374d003d6ee9d21977e49e050547
Jérôme JACOB <jerome56jj@hotmail.com>
2022-03-22 12:56:47 +0100

    WorkingWithTime

---
+++ WorkingWithTime/Program.cs
@@ -0,0 +1,13 @@
+﻿using static System.Console;
+
+WriteLine("Earliest date/time value is: {0}",
+    arg0: DateTime.MinValue);
+
+WriteLine("UNIX epoch date/time value is: {0}",
+    arg0: DateTime.UnixEpoch);
+
+WriteLine("Date/time value Now is: {0}",
+    arg0: DateTime.Now);
+
+WriteLine("Date/time value Today is: {0}",
+    arg0: DateTime.Today);
