916b0d491bcffdcec3be88cd45811f524afe0aac
Jérôme JACOB <jerome56jj@hotmail.com>
2022-03-22 16:33:31 +0100

    WorkingWithTime

--- WorkingWithTime/Program.cs
+++ WorkingWithTime/Program.cs
@@ -11,3 +11,20 @@ WriteLine("Date/time value Now is: {0}",
 
 WriteLine("Date/time value Today is: {0}",
     arg0: DateTime.Today);
+
+WriteLine();
+
+DateTime christmas = new(year: 2022, month: 12, day: 25);
+
+WriteLine("Christmas: {0}",
+    arg0: christmas); // default format
+
+WriteLine("Christmas: {0:dddd, dd MMMM yyyy}",
+    arg0: christmas); // custom format
+
+WriteLine("Christmas is day {0} of the year.",
+    arg0: christmas.DayOfYear);
+
+WriteLine("Christmas {0} is on a {1}.",
+    arg0: christmas.Year,
+    arg1: christmas.DayOfWeek);
