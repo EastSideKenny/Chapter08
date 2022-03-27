3d6b326117250bd5d3cdc7894fb535ce26cdf450
Jerome Jacob <Jerome56jj@hotmail.com>
2022-03-27 22:55:30 +0200

    Working with Collections - Lists

---
+++ WorkingWithCollections/Program.cs
@@ -0,0 +1,41 @@
+﻿using static System.Console;
+
+WorkingWithLists();
+
+static void Output(string title, IEnumerable<string> collection)
+{
+    WriteLine(title);
+    foreach (string item in collection)
+    {
+        WriteLine($"    {item}");
+    }
+}
+
+static void WorkingWithLists()
+{
+    // Simple syntax for creating a list and adding three items
+    List<string> cities = new();
+    cities.Add("London");
+    cities.Add("Paris");
+    cities.Add("Luxembourg");
+
+    /* Alternative syntax that is converted by the compiler into
+       the three Add method calls above
+       List<string> cities = new()
+        { "London", "Paris", "Luxembourg" };
+     */
+
+    Output("Initial list", cities);
+
+    WriteLine($"The first city is {cities[0]}.");
+    WriteLine($"The last city is {cities[cities.Count - 1]}.");
+
+    cities.Insert(0, "Sydney");
+
+    Output("After inserting Sydney at index 0", cities);
+
+    cities.RemoveAt(1);
+    cities.Remove("Luxembourg");
+
+    Output("After removing two cities", cities);
+}
