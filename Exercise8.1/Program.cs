5345d2493f900a0f0f1e8cea3ef4ad3ab72f9b4b
Jerome Jacob <Jerome56jj@hotmail.com>
2022-04-01 22:28:34 +0200

    Exercies 8.2

---
+++ Exercise8.1/Program.cs
@@ -0,0 +1,20 @@
+﻿using static System.Console;
+using System.Text.RegularExpressions;
+
+WriteLine("The default regular expression checks for at least one digit.");
+Write("Enter a regular expression (or press ENTER to use the default): ");
+string? regInput = ReadLine();
+Regex regEx;
+
+if (!String.IsNullOrEmpty(regInput))
+{
+    regEx = new(regInput);
+}else
+{
+    regEx = new("^[0-9]{1,}");  // correct regex : ^ <- Start of input / \d <- single digit / + <- one or more / $ <- end of input
+}
+
+WriteLine();
+Write("Enter some input: ");
+string? input = ReadLine();
+WriteLine($"{input} matches {regEx.ToString()} {regEx.IsMatch(input)}");
